8cc083f6ede51cf94c7e20068814d39701feb3fe
Akram Zaki <akram.zaki@grenoble-inp.org>
2022-03-21 08:13:38 +0000

    Update rebouclage.s

--- program/autotest/rebouclage.s
+++ program/autotest/rebouclage.s
@@ -1,9 +1,10 @@
 # TAG = rebouclage
 
 	.text
-	lui x31,0xFFFFF
-    .word 0xFFFFFFFF
+	lui x31,0x00055
+    .word 0x00015601
     lui x31,0x12345
+	ADDI
     # max_cycle 250
 	# pout_start
 	# FFFFF000
