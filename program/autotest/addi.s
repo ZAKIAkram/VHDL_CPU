255797ce3161e1f205d3e2633677d6d84b05ac89
Akram Zaki <zakiak@ensipc183.ensimag.fr>
2022-03-21 14:46:41 +0100

    addi

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -1,9 +1,9 @@
 # TAG = addi 
 	.text
 
-    addi x4, x0, 5
-    addi x31, x4, 6
-    	#max cycle 50
+    addi x4, x0, 0x0005
+    addi x31, x4, 0x0006
+        #max cycle 50
     	#pout_start
     	#0000000B
     	#pout_end
