d64b1e56c607e370e23d7fa595cb812d3ca1a4fc
Akram Zaki <zakiak@ensipc102.ensimag.fr>
2022-03-21 14:25:06 +0100

    test2

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -3,9 +3,9 @@
 
     addi x31, x0, 5
     addi x31, x0, 6
-    #max cycle 50
-    #pout_start
-    #0000000B
-    #pout_end
+    	#max cycle 50
+    	#pout_start
+    	#0000000B
+    	#pout_end
 
 
