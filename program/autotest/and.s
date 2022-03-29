4bd58dcbd612f0bc56f31745be792d164421cb92
Akram Zaki <zakiak@ensipc589.ensimag.fr>
2022-03-29 18:03:08 +0200

    and

--- program/autotest/and.s
+++ program/autotest/and.s
@@ -1,12 +1,11 @@
 # TAG = and
         .text
 
-    li x31, 1
-    li x30, 0
-    and x31, x31, x30
+    li x4, 1
+    li x5, 1
+    and x31, x4, x5
         #max cycle 50
         #pout_start
         #00000001
-        #00000000
         #pout_end
 
