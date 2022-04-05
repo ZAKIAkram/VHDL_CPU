a012299dd3e61289e5c870d55622547e7ce52da4
Ali Kohen <ali.kohen@grenoble-inp.org>
2022-04-05 21:53:32 +0200

    Update 3

--- program/autotest/beq.s
+++ program/autotest/beq.s
@@ -1,7 +1,7 @@
 # TAG = beq
         .text
-    addi x4, x0, 0x0011 
-    addi x5, x0, 0x0011
+    addi x4, x0, 5 
+    addi x5, x0, 5
     addi x31, x0, 0x0000
     beq x4, x5, saut_beq
     
@@ -12,6 +12,6 @@ saut_beq:
         #max cycle 50
         #pout_start
         #00000000
-        #00000011
+        #00000005
         #pout_end
 
