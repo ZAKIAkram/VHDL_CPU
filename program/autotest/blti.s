3df354fecc68eb4dda8f3e8a8689d033fb2a645c
Ali Kohen <ali.kohen@grenoble-inp.org>
2022-04-05 20:17:50 +0200

    Update 3

--- program/autotest/blti.s
+++ program/autotest/blti.s
@@ -1,17 +1,17 @@
 # TAG = blti
         .text 
         
-        
-    addi x31, x0, 0
-for_blti:
-    addi x4, x0, 17
-    addi x31, x31, 1
-    blti x4, 3, for_blti
-    
 
-        #max cycle 500
+    addi x4, x0, 0
+    blti x4, 1, for
+sauter:
+    addi x31, x0, 1
+for:
+    addi x31, x0, 3
+    
+ 
+
+        #max cycle 50
         #pout_start
-        #00000000
-        #00000001
         #00000003
         #pout_end
--- program/autotest/slt.s
+++ program/autotest/slt.s
@@ -5,27 +5,9 @@
 addi x4, x0, 5
 slt x31, x4, x4
 
-# -1 < 1
-addi x3, x0, -1
-addi x4, x0, 1
-slt x31, x3, x4
-
-# 8 => 1
-addi x3, x0, 8
-addi x4, x0, 1
-slt x31, x3, x4
-
-# -10 < -8
-addi x3, x0, -10
-addi x4, x0, -8
-slt x31, x3, x4
-
 # max_cycle 50
 # pout_start
 # 00000000
-# 00000001
-# 00000000
-# 00000001
 # pout_end
 
 
