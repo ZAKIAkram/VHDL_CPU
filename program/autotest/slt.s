1a5fd2bf0bf6719473afc555052a3a63803a2aba
Ali Kohen <ali.kohen@grenoble-inp.org>
2022-04-06 00:09:26 +0200

    Update 3

--- program/autotest/slt.s
+++ program/autotest/slt.s
@@ -1,21 +1,29 @@
 # TAG = slt
 .text
 
+# 5 => 5
 addi x4, x0, 5
-addi x5, x0, 9
-slt x31, x4, x5
+slt x31, x4, x4
 
-addi x5, x5, -11
-slt x31, x4, x5
+# -1 < 1
+addi x3, x0, -1
+addi x4, x0, 1
+slt x31, x3, x4
 
-addi x5, x5, -11
-slt x31, x4, x5
+# 8 => 1
+addi x3, x0, 8
+addi x4, x0, 1
+slt x31, x3, x4
 
-# max_cycle 100
+# -10 < -8
+addi x3, x0, -10
+addi x4, x0, -8
+slt x31, x3, x4
+
+# max_cycle 50
 # pout_start
+# 00000000
 # 00000001
 # 00000000
 # 00000001
 # pout_end
-
-
