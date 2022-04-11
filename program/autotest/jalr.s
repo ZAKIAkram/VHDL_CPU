9ac4c1a889586341131cc5f454df78b2ad0ad352
Akram Zaki <zakiak@pcserveur.ensimag.fr>
2022-04-12 01:09:07 +0200

    jalr again

--- program/autotest/jalr.s
+++ program/autotest/jalr.s
@@ -8,8 +8,10 @@
             addi x31, x31, 0x0003
         fin:
             addi x4, x0, 0x0000
+            jalr x5
         #max_cycle 50
         #pout_start
         #00000001
         #00000004
+        #00000007
         #pout_end
