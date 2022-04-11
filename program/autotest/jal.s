433d50321c5d58773d571651b914b30c5c092128
Akram Zaki <zakiak@pcserveur.ensimag.fr>
2022-04-12 00:10:45 +0200

    test JAL

---
+++ program/autotest/jal.s
@@ -0,0 +1,17 @@
+#TAG = jal
+
+        .text
+    addi x31, x0, 0x0004
+    jal x0, addition
+    j fin
+
+    addition:
+        addi x31, x31, 0x0004
+        ret
+    fin:
+        addi x4, x0, 0x0000
+        #max_cycle 50
+        #pout_start
+        #00000004
+        #00000008
+        #pout_end
