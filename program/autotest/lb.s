4ebc29d16964f26686decce20084b10adcc881b9
Ali Kohen <ali.kohen@grenoble-inp.org>
2022-04-12 06:13:05 +0000

    Update lb.s

--- program/autotest/lb.s
+++ program/autotest/lb.s
@@ -1,12 +1,11 @@
-#TAG = lb
-        .text
-    addi x4, x0, 556
-    auipc x5, 0
-    sw x4, 0(x5)
-    lb x31, 0(x5)
+# TAG = lb
+.text
+addi x29, x0, 255
+auipc x30, 0
+sw x29, 0(x30)
+lb x31, 0(x30)
 
-        
-        #max_cycle 50
-        #pout_start
-        #0000002C
-        #pout_end
+# max_cycle 50
+# pout_start
+# FFFFFFFF
+# pout_end
