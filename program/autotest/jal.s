6c4c2d4b16955687c9bd990c37f6cad314513382
Akram Zaki <zakiak@pcserveur.ensimag.fr>
2022-04-12 02:08:31 +0200

    bien

--- program/autotest/jal.s
+++ program/autotest/jal.s
@@ -1,18 +1,14 @@
 #TAG = jal
 
         .text
-    addi x31, x0, 0x0004
-    jal x0, addition
-    j fin
-
-    addition:
-        addi x31, x31, 0x0004
-        addi ra, x0, 0x0000
-        ret
-    fin:
-        addi x4, x0, 0x0000
+        #PC est initialisée à 0x1000
+    addi x31, x0, 0x0001
+    jal x4, test_1 #x4 va valoir 0x1004 (PC+4)
+    addi x31,x0, 0x0002
+    test_1:
+        addi x31, x31, 0x0001
         #max_cycle 50
         #pout_start
-        #00000004
-        #00000008
+        #00000001
+        #00000002
         #pout_end
